d01b8fa8befefb12043f8858b7b008bf587ea85f
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-08 17:22:12 -0600

    added selectAll and insertOne functions to orm

---
+++ config/orm.js
@@ -0,0 +1,25 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js"); // ./connection.js?
+
+const orm = {
+    selectAll: function(table, cb){
+        const queryString = "SELECT * FROM " + table;
+        connection.query(queryString,function(err,result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    // INSERT INTO burgers (burger_name,devoured) VALUES (?,?)
+    insertOne: function(table,val,cb){
+        const queryString = "INSERT INTO " + table + " (burger_name,devoured) VALUES (?,?)";
+        connection.query(queryString,val,function(err,result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(){
+        console.log("update!");
+    }
+};
+
+module.exports = orm;
