e33f4c69682fa109f189f130a916427b1a53d655
James Leitschuh <james.leitschuh02@gmail.com>
2021-01-08 16:56:50 -0600

    database burgers_db created and properly seeded

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("Burger-1",true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Burger-2",false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Burger-3",true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Burger-4",false);
